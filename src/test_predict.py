e1be6609998d2ea85f43acc20e7d3fbc06dacc73
anggoro-bimo <bimoanggorop@gmail.com>
2023-04-21 01:15:40 +0700

    modified, prediction succeed

--- src/test_predict.py
+++ src/test_predict.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sys
 sys.path.append('/root/ml_process_feb23/')
 import src.util as utils
+import src.preprocessing as preprocessing
+import src.modelling as modelling
 
 if __name__ == "__main__":
 
@@ -11,23 +13,41 @@ if __name__ == "__main__":
     # Loading production model
     model_data = utils.pkl_load(config["production_model_path"])
 
-    # Define the prodictors
+    # Define the predictors
     data = {
-        "reports": [],
-        "age": [],
-        "income": [],
-        "share": [],
-        "expenditure": [],
-        "owner": [],
-        "selfemp": [],
-        "dependents": [],
-        "months": [],
-        "majorcards": [],
-        "active": []
+        "reports": [2],
+        "age": [20],
+        "income": [30],
+        "share": [.2],
+        "expenditure": [700],
+        "owner": ["no"],
+        "selfemp": ["no"],
+        "dependents": [2],
+        "months": [30],
+        "majorcards": [1],
+        "active": [4]
     }
 
     # Convert to DataFrame
-    predictors = pd.DataFarame(data)
+    predictors = pd.DataFrame(data)
+
+    # Data transformation
+    predictors = preprocessing.cols_transform(predictors, config["cols_to_log"])
+
+    # Label encoding
+    predictors = preprocessing.label_encoding(predictors, config["predictors_cat"])
+
+    # Data binning
+    predictors = preprocessing.binning(predictors, 'age', config["bins_age"], config["labels_age"])
+    predictors = preprocessing.binning(predictors, 'reports', config["bins_reports"], config["labels_reports"])
+    predictors = preprocessing.binning(predictors, 'dependents', config["bins_dependents"], config["labels_dependents"])
+    predictors = preprocessing.binning(predictors, 'active', config["bins_active"], config["labels_active"])
+
+    # Include the binned features for fitting the model
+    predictors = modelling.get_binned_features(predictors, config["original_cols"])
+
+    # Value division
+    predictors = preprocessing.division(predictors, 'income_log')
 
     # Check the result
     print(model_data.predict(predictors))
